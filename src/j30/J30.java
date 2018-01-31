d69a0d48f1c0dc859537fee1d00e015bdbf60c1c
zhumin0718 <994808910@qq.com>
2018-01-31 13:24:50 +0800

    first

---
+++ src/j30/J30.java
@@ -0,0 +1,17 @@
+package j30;
+
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthToggleButtonUI;
+
+public class J30 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in) ;
+		String str=scanner.next();
+		System.out.println("接收到的内容是"+str);
+		
+		/*String str1="234";
+		String str2="46";
+		System.out.println(Integer.parseInt(str1)+Integer.parseInt(str2));*/
+	}
+}
